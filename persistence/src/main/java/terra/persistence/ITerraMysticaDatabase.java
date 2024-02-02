84c7ef742a0ffcd06ce8dc6d42bad78707970ddf
Daniel Pols <dpols@sogyo.nl>
2024-02-02 15:56:41 +0100

    Added (in-memory) database saving and loading

--- persistence/src/main/java/terra/persistence/ITerraMysticaDatabase.java
+++ persistence/src/main/java/terra/persistence/ITerraMysticaDatabase.java
@@ -1,7 +1,13 @@
 package terra.persistence;
 
+import terra.domain.ITerraMystica;
+
 public interface ITerraMysticaDatabase {
 
     String getStartingBoard();
+
+    void saveGame(String id, ITerraMystica game);
+
+    ITerraMystica loadGame(String id);
 
 }
--- persistence/src/main/java/terra/persistence/ITerraMysticaRepository.java
+++ persistence/src/main/java/terra/persistence/ITerraMysticaRepository.java
@@ -1,8 +1,13 @@
 package terra.persistence;
 
+import terra.domain.ITerraMystica;
 import terra.domain.Terrain;
 
 public interface ITerraMysticaRepository {
 
     Terrain[] getStartingTerrain();
+
+    void saveGame(String id, ITerraMystica game);
+
+    ITerraMystica loadGame(String id);
 }
