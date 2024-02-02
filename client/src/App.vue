07373522a5cd4f171115ecea48126f693ed1d2f3
Daniel Pols <dpols@sogyo.nl>
2024-02-02 14:40:44 +0100

    Added screen to input player names and terrain types

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import TerraMystica from './components/TerraMystica.vue'
+import StartScreen from './components/StartScreen.vue';
 import { gameState } from './global.js';
 </script>
 
 <template>
   <main>
-    <template v-if="swap"><TerraMystica/></template><br/>
+    <template v-if="swap"><TerraMystica/></template>
+    <template v-else><StartScreen/></template>
+    <br/>
     <button type="button" class="switchButton" v-on:click="swap = !swap">Hi {{ swap }}</button>
     <button type="button" v-on:click="print">Log</button>
     <button type="button" v-on:click="getGameState">Fetch!</button>
---
+++ client/src/components/StartScreen.vue
@@ -0,0 +1,59 @@
+<script setup>
+    import { VueElement, reactive } from 'vue';
+</script>
+
+<template>
+    <div>
+        <ul class="playerList">
+            <li v-for="(player, i) in playerList" :key="player">
+                <input class="playerNameInput" type="text" @input="event => playerList[i].name = event.target.value">
+                <select class="playerTerrainInput" @input="event => playerList[i].terrain = event.target.value">
+                    <option value="PLAINS">Plains</option>
+                    <option value="SWAMP">Swamp</option>
+                    <option value="LAKE">Lake</option>
+                    <option value="FOREST">Forest</option>
+                    <option value="MOUNTAINS">Mountains</option>
+                    <option value="WASTELAND">Wasteland</option>
+                    <option value="DESERT">Desert</option>
+                </select>
+                <button v-if="playerList.length > 2" type="button" @click="playerList.splice(i, 1)">Remove</button>
+            </li>
+            <li v-if="playerList.length < 5"><button type="button" @click="addPlayer">Add another player</button></li>
+            <li><button type="button" @click="logPlayers">We do a little logging</button></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                playerList: []
+            };
+        },
+        methods: {
+            addPlayer() {
+                this.playerList.push({
+                    name: "",
+                    terrain: "PLAINS"
+                });
+            },
+            logPlayers() {
+                console.log(this.playerList);
+            },
+            removePlayer(index) {
+                this.$delete(this.playerList, index);
+            }
+        },
+        beforeMount() {
+            this.addPlayer();
+            this.addPlayer();
+        }
+    }
+</script>
+
+<style scoped>
+.playerList {
+    list-style-type: none;
+}
+</style>
