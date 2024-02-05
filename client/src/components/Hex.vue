1f852b24d7e52700c6c6df6fbf2b2b9fcfb41096
Daniel Pols <dpols@sogyo.nl>
2024-02-05 11:22:09 +0100

    Added css from file

--- client/src/components/Hex.vue
+++ client/src/components/Hex.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+    import '../css/hexcss.css'
 </script>
 
 <template>
@@ -53,59 +53,6 @@
   }
 </script>
 
-<style scoped>
-.tileListItem {
-    position: relative;
-    display: inline;
-    box-sizing: border-box;
-    grid-column-end: span 2;
-    outline: 1px solid transparent;
-    border-bottom: 1px solid transparent;
-    width: 100%;
-    padding-bottom: 86.602%;
-}
-
-.tileListItem * {
-    margin: 0;
-    padding: 0;
-}
-
-.tileListItem:nth-child(25n+1) {
-    grid-column-start: 1;
-}
-
-.tileListItem:nth-child(25n+14) {
-    grid-column-start: 2;
-}
-
-.tileContainer, .tileContainer * {
-    position: absolute;
-}
-
-.tileContainer {
-    overflow: hidden;
-    visibility: hidden;
-    width: 100%;
-    padding-bottom: 115.47%;
-    transform: rotate3d(0, 0, 1, -60deg) skewY(30deg);
-}
-
-.tile {
-    display: block;
-    overflow: hidden;
-    visibility: visible;
-    width: 100%;
-    height: 100%;
-    transform: skewY(-30deg) rotate3d(0, 0, 1, 60deg);
-}
-
-.tileButton {
-    border-width: 0;
-    width: 100%;
-    height: 100%;
-    background-color: var(--bg-color);
-    color: var(--text-color);
-    font-size: 8px;
-}
+<style scoped src="../css/hexcss.css">
 </style>
 
