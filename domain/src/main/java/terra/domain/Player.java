7e1bd73ea7e7827c0084c70cac95dc774a7741df
Daniel <dpols@sogyo.nl>
2024-02-05 08:12:30 +0100

    Added simple player class to hold player information

---
+++ domain/src/main/java/terra/domain/Player.java
@@ -0,0 +1,13 @@
+package terra.domain;
+
+public class Player {
+
+    String name;
+    Terrain terrain;
+
+    public Player(String name, Terrain terrain) {
+        this.name = name;
+        this.terrain = terrain;
+    }
+
+}
