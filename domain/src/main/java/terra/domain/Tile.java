bcc6c005ff7355f1d0e7d1ebcbd2ad9c01f4a850
Daniel Pols <dpols@sogyo.nl>
2024-02-06 14:36:28 +0100

    Made all non-interface methods protected to ensure interface usage

--- domain/src/main/java/terra/domain/Tile.java
+++ domain/src/main/java/terra/domain/Tile.java
@@ -8,44 +8,44 @@ public class Tile {
     private Terrain terrain;
     private List<Tile> adjacent;
 
-    public Tile(TileLocation location, Terrain terrain) {
+    protected Tile(TileLocation location, Terrain terrain) {
         this.location = location;
         this.terrain = terrain;
     }
 
-    public void setAdjacent(List<Tile> others) {
+    protected void setAdjacent(List<Tile> others) {
         this.adjacent = others.stream().filter(t -> this.isAdjacentTo(t))
                 .toList();
     }
 
-    public boolean isAdjacentTo(Tile other) {
+    protected boolean isAdjacentTo(Tile other) {
         return location.isAdjacentTo(other.getLocation());
     }
 
-    public TileLocation getLocation() {
+    protected TileLocation getLocation() {
         return location;
     }
 
-    public Terrain getTerrain() {
+    protected Terrain getTerrain() {
         return terrain;
     }
 
-    public List<Tile> getAdjacent() {
+    protected List<Tile> getAdjacent() {
         return adjacent;
     }
 
 }
 
 record TileLocation(int row, int col) {
-    public int[] toArray() {
+    protected int[] toArray() {
         return new int[] { row, col };
     }
 
-    public static TileLocation fromArray(int[] array) {
+    protected static TileLocation fromArray(int[] array) {
         return new TileLocation(array[0], array[1]);
     }
 
-    public static TileLocation fromBoardIndex(int index, int size) {
+    protected static TileLocation fromBoardIndex(int index, int size) {
         // Alternating rows of size and (size-1)
         int doubleRow = index / (2 * size - 1);
         int singleRow = (index - (2 * size - 1) * doubleRow) / size;
@@ -54,13 +54,13 @@ record TileLocation(int row, int col) {
         return new TileLocation(row, col);
     }
 
-    public int compare(TileLocation other) {
+    protected int compare(TileLocation other) {
         return Integer.compare(row, other.row) == 0
                 ? Integer.compare(col, other.col)
                 : Integer.compare(row, other.row);
     }
 
-    public boolean isAdjacentTo(TileLocation other) {
+    protected boolean isAdjacentTo(TileLocation other) {
         if (other.row - row == 0 && Math.abs(other.col - col) == 1) {
             return true;
         }
