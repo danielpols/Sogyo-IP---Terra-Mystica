740dbb272da71a991600e07ac08f15b754df5db7
Daniel Pols <dpols@sogyo.nl>
2024-02-12 10:52:41 +0100

    Restricted pan limits

--- client/src/components/TerraMystica.vue
+++ client/src/components/TerraMystica.vue
@@ -34,40 +34,40 @@ export default {
             const oldZoom = this.boardZoomLevel;
 
             this.boardZoomLevel *= 1-event.deltaY/1000;
-            if(this.boardZoomLevel < 0.5) {
-                this.boardZoomLevel = 0.5;
+            if(this.boardZoomLevel < 0.6) {
+                this.boardZoomLevel = 0.6;
             }
             if(this.boardZoomLevel > 3) {
                 this.boardZoomLevel = 3;
             }
 
-            console.log(mouseX(this.boardZoomLevel), mouseY(this.boardZoomLevel));
             this.boardOffset.x = unpannedDX(this.boardZoomLevel)-this.boardZoomLevel*mouseX(oldZoom);
             this.boardOffset.y = unpannedDY(this.boardZoomLevel)-this.boardZoomLevel*mouseY(oldZoom);
+
+            this.restrictPan();
         },
         panBoard(event) {
             this.boardOffset.x += event.movementX;
             this.boardOffset.y += event.movementY;
-
-            const paddingX = Math.abs((this.$refs.boardContainer.clientWidth - this.$refs.board.$el.clientWidth * this.boardZoomLevel)/2);
-
-            const offsetY = this.$refs.board.$el.clientWidth * 0.0222; // offset from margin
-            const paddingY = Math.abs((this.$refs.boardContainer.clientHeight - (this.$refs.board.$el.clientHeight + offsetY) * this.boardZoomLevel)/2);
-
-            console.log(this.boardPanMargins.x, this.boardPanMargins.y)
-
-            if(Math.abs(this.boardOffset.x) > paddingX) {
-                this.boardOffset.x = Math.sign(this.boardOffset.x) * paddingX;
+            this.restrictPan();
+        },
+        restrictPan() {
+            if(Math.abs(this.boardOffset.x) > this.boardPanMargins.x) {
+                this.boardOffset.x = Math.sign(this.boardOffset.x) * this.boardPanMargins.x;
             }
-
-            if(Math.abs(this.boardOffset.y) > paddingY) {
-                this.boardOffset.y = Math.sign(this.boardOffset.y) * paddingY;
+            if(Math.abs(this.boardOffset.y) > this.boardPanMargins.y) {
+                this.boardOffset.y = Math.sign(this.boardOffset.y) * this.boardPanMargins.y;
             }
         }
     },
     mounted() {
         this.boardPanMargins.x = this.$refs.boardContainer.clientWidth / 4;
         this.boardPanMargins.y = this.$refs.boardContainer.clientHeight / 4;
+    },
+    beforeUpdate() {
+        this.boardPanMargins.x = this.$refs.board.$el.clientWidth*this.boardZoomLevel/2 - this.$refs.boardContainer.clientWidth/4;
+        const offsetY = this.$refs.board.$el.clientWidth * 0.0222; // offset from margin
+        this.boardPanMargins.y = this.$refs.board.$el.clientHeight*this.boardZoomLevel/2 + offsetY - this.$refs.boardContainer.clientHeight/4;
     }
 }
 </script>
