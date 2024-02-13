a52fcf399e8fac471bb7c4891508f9e5fde13797
Daniel Pols <dpols@sogyo.nl>
2024-02-13 13:53:27 +0100

    Removed location information from TileDTO

--- api/src/main/java/terra/api/models/TileDTO.java
+++ api/src/main/java/terra/api/models/TileDTO.java
@@ -6,20 +6,14 @@ import terra.domain.Terrain;
 
 public class TileDTO {
 
-    private int[] location;
     private Terrain terrain;
     private Building building;
     private boolean buildable;
 
     public TileDTO(ITerraMystica game, int[] location) {
-        this.location = new int[] { location[0], location[1] };
         this.terrain = game.getTileTerrain(location);
         this.building = game.getTileBuilding(location);
         this.buildable = game.tileIsBuildable(location);
-    }
-
-    public int[] getLocation() {
-        return location;
     }
 
     public Terrain getTerrain() {
