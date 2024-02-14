118d64c3f2ad07bf6911fd792ba18353328e9aac
Daniel Pols <dpols@sogyo.nl>
2024-02-14 14:30:41 +0100

    Added passAction to playerDTO

--- api/src/main/java/terra/api/models/PlayerDTO.java
+++ api/src/main/java/terra/api/models/PlayerDTO.java
@@ -8,11 +8,19 @@ public class PlayerDTO {
     private String name;
     private Terrain terrain;
     private boolean turn;
+    private boolean passed;
+    private boolean starting;
+
+    private ActionDTO passAction;
 
     public PlayerDTO(ITerraMystica game, String name) {
         this.name = name;
         this.terrain = game.getPlayerTerrain(name);
         this.turn = game.playerHasTurn(name);
+        this.passed = game.playerHasPassed(name);
+        this.starting = game.isStartingPlayer(name);
+
+        this.passAction = ActionDTO.getActionDTO(game.getPassAction(name));
     }
 
     public String getName() {
@@ -25,6 +33,18 @@ public class PlayerDTO {
 
     public boolean getTurn() {
         return this.turn;
+    }
+
+    public boolean getPassed() {
+        return this.passed;
+    }
+
+    public boolean getStarting() {
+        return this.starting;
+    }
+
+    public ActionDTO getPassAction() {
+        return this.passAction;
     }
 
 }
